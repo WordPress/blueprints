1bea9903b934e57091df33517d50f61a47308b2b
JuanMa Garrido <juanma.garrido@gmail.com>
2024-08-01 11:10:57 +0100

    import funtion as mu-plugin

---
+++ blueprints/reset-data-and-import-content/log-content-state.php
@@ -0,0 +1,26 @@
+<?php
+
+require "/wordpress/wp-load.php";
+
+function log_content_state($message)
+{
+    $args = [
+        "post_type" => "post",
+        "post_status" => "publish",
+        "posts_per_page" => -1,
+    ];
+
+    $posts = get_posts($args);
+    error_log($message);
+
+    if (!empty($posts)) {
+        $last_post = $posts[count($posts) - 1];
+        error_log(
+            count($posts) .
+                " posts - Last Post title: " .
+                $last_post->post_title
+        );
+    } else {
+        error_log("No posts found");
+    }
+}

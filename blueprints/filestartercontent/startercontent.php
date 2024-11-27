c3a51713ff1790a746ee5a7a5c03d3731a763cc4
Birgit Pauli-Haack <birgit.pauli@gmail.com>
2024-11-27 15:05:04 +0100

    unescaped double-quotes

--- blueprints/filestartercontent/startercontent.php
+++ blueprints/filestartercontent/startercontent.php
@@ -7,13 +7,27 @@ Version: 0.0.1
 */
 add_action( 'after_setup_theme', function() {
     add_theme_support( 'starter-content', array(
-		'posts'     => array(
-			'homepage-section' => array(
-				'post_type'    => 'page',
-				'post_title'   => _x( 'Subscriptions', 'Theme starter content' ),
-				'post_content' => sprintf( '<!-- wp:group {"layout":{"type":"constrained"}} -->\n<div class=\"wp-block-group\"><!-- wp:heading {"textAlign":"center"} -->\n<h2 class=\"wp-block-heading has-text-align-center\">Our Subscriptions</h2>\n<!-- /wp:heading -->\n<!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":"1.125rem"},"spacing":{"margin":{"top":"var:preset|spacing|10"}}}} -->\n<p class=\"has-text-align-center\" style=\"margin-top:var(--wp--preset--spacing--10);font-size:1.125rem\">We offer two subscription levels: </p>\n<!-- /wp:paragraph -->\n<!-- wp:list -->\n<ul class=\"wp-block-list\"><!-- wp:list-item -->\n<li><strong><em>Free</em> at $0</strong> - Access to 5 exclusive <em>Études Articles</em> per month.</li>\n<!-- /wp:list-item -->\n<!-- wp:list-item -->\n<li><strong><em>Connoisseur</em> at $12</strong> - Access to 20 exclusive <em>Études Articles</em> per month.<br></li>\n<!-- /wp:list-item --></ul>\n<!-- /wp:list --></div>\n<!-- /wp:group -->',''
-					, 'Theme starter content' )
-				)
-		)
-    ) )
-};
+        'posts' => array(
+            'homepage-section' => array(
+                'post_type'    => 'page',
+                'post_title'   => _x( 'Subscriptions', 'Theme starter content', 'your-text-domain' ),
+                'post_content' => '<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:heading {"textAlign":"center"} -->
+<h2 class="wp-block-heading has-text-align-center">Our Subscriptions</h2>
+<!-- /wp:heading -->
+<!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":"1.125rem"},"spacing":{"margin":{"top":"var:preset|spacing|10"}}}} -->
+<p class="has-text-align-center" style="margin-top:var(--wp--preset--spacing--10);font-size:1.125rem">We offer two subscription levels: </p>
+<!-- /wp:paragraph -->
+<!-- wp:list -->
+<ul class="wp-block-list"><!-- wp:list-item -->
+<li><strong><em>Free</em> at $0</strong> - Access to 5 exclusive <em>Études Articles</em> per month.</li>
+<!-- /wp:list-item -->
+<!-- wp:list-item -->
+<li><strong><em>Connoisseur</em> at $12</strong> - Access to 20 exclusive <em>Études Articles</em> per month.<br></li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list --></div>
+<!-- /wp:group -->'
+            ),
+        ),
+    ) );
+} );
